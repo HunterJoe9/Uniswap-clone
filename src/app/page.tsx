b185891beb5c57353a42748a5b9da3dd0daf31a0
HunterJoe <hunterjoe9999@gmail.com>
2023-10-05 12:32:59 +0900

    uniswap front template

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+"use client"
+import Header from "@/components/Header"
+import Main from "@/components/Main"
+import Bottom from "@/components/Bottom"
+
+
+export default function Home() {
+  return (
+    <div className="h-screen max-h-screen h-min-screen w-screen bg-[#2D242F] text-white select-none flex flex-col justify-between">
+      <Header/>
+      <Main/>
+      <Bottom/>
+    </div>
+  )
+}
---
+++ src/components/Bottom.tsx
@@ -0,0 +1,13 @@
+import { RxDotFilled } from "react-icons/rx"
+
+const Bottom = () => {
+    return(
+        <div className="flex text-[#40B66B] justify-end mx-2 py-2">
+            <div className="text-[11px]">18281544</div>
+            <RxDotFilled />
+        </div>
+
+    )
+}
+
+export default Bottom;
